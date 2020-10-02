3c4c2b1438723765efcb1b2f86e9792cca68f90d
Luce <luce@ohio.edu>
2020-10-02 09:53:21 -0400

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace luceMIS4200demo1.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+
+        [Display (Name ="Customer First Name")]
+        public string firstName { get; set; }
+
+        [Display (Name ="Customer Last Name")]
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        [Display (Name ="Membership Date")]
+        public DateTime customerSince { get; set; }
+        public ICollection<Order> order { get; set; }
+
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(luceMIS4200demo1.Startup))]
+namespace luceMIS4200demo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
